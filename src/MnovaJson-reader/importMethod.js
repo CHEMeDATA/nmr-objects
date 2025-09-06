e857a9ccee9bf37c39a4b2449bf7eb4a28b69e25
djeanner <djeanner@users.noreply.github.com>
2025-09-06 19:42:15 +0200

    update with larmor in spectra

--- src/MnovaJson-reader/importMethod.js
+++ src/MnovaJson-reader/importMethod.js
@@ -62,6 +62,7 @@
 			}
 
 			var input = extractSpectrumData(allSpectraObjectsExtracted[0][0], "data");
+			var larmor = input.larmor;
 			if (param) {
 				if (param.filterSpectra) {
 					if (param.filterSpectra == "onlyFirst") {
@@ -130,6 +131,7 @@
 
 			// create final data object
 			this.data = {
+				frequency: larmor,
 				values: values,
 				firstPoint: extremas_chemshift.max,
 				lastPoint: extremas_chemshift.min,
