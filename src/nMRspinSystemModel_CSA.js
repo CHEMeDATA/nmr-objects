a31364c23dab83e66b9a3f09e4b6906e3481f910
djeanner <djeanner@users.noreply.github.com>
2025-09-18 10:50:44 +0200

    update csa

--- src/nMRspinSystemModel_CSA.js
+++ src/nMRspinSystemModel_CSA.js
@@ -1,4 +1,5 @@
 // AUTOMATIC IMPORT INSERTION WILL BE MADE HERE
+// include for bridge from solidStateNMRCSA-reader
 
 import { ObjectBase } from "./objectBase.js";
 
@@ -9,11 +10,37 @@ export class NMRspinSystemModel_CSA extends ObjectBase {
 		this.verbose = 0;
 	}
 
-	_handleLoadDemoData(input) {
-		this._loadDemoData(input);
+	_handleLoadDemoData(demoParam) {
+		this._loadDemoData(demoParam);
 	}
 
-	_loadDemoData(input) {
+	_loadDemoData(demoParam = {}) {
+		var spins = [];
+		for (const elem of demoParam) {
+			const diagTensorValues = {
+				wildComment:
+					"Created by schema/scripts/createSchemaSomeInstances.js using function createInstance",
+				xx: elem[0],
+				yy: elem[1],
+				zz: elem[2],
+			};
+			const spin = {
+				wildComment:
+					"Created by schema/scripts/createSchemaSomeInstances.js using function createInstance",
+				typeVariableString: "ChemicalShift",
+				diagTensorValues: diagTensorValues,
+			};
+			spins.push(spin);
+		}
+
+		const full = {
+			$schema:
+				"https://chemedata.github.io/schema/v1/schema/NMRspinSystemModel_CSA.json",
+			wildComment: "Created by demo option ",
+			spins: spins,
+		};
+
+		this.data = full;
 	}
 
 // AUTOMATIC METHOD INSERTION WILL BE MADE HERE
